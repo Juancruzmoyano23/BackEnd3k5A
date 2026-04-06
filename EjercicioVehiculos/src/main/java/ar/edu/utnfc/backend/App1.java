415a10645fce2f66b8b2832b8c0bbdf04a2e8a2f
Juan Cruz Moyano <juancruzmoyanoleonangeli23@gmail.com>
2026-04-05 21:22:16 -0300

    ejercicio vehiculo modificate

--- EjercicioVehiculos/src/main/java/ar/edu/utnfc/backend/App1.java
+++ EjercicioVehiculos/src/main/java/ar/edu/utnfc/backend/App1.java
@@ -8,14 +8,6 @@ public class App1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-
-        Vehiculos v = new Autos("Ford", "Focus");
-        v.mostrarIdentificacion();
-
-        Motos m = new Motos("Honda", "CBR");
-        Barco b = new Barco(100);
-        Avion a = new Avion(10000);
-
         List<Vehiculos> lista = new ArrayList<>();
         lista.add(new Autos("Toyota", "Corolla"));
         lista.add(new Motos("Yamaha", "R1"));
